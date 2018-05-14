f4de1ac3b05f198245f1b880f1e13e3787f79eac
wwwWangwg <413411142@qq.com>
2018-05-14 16:23:34 +0800

    精简代码 sql脚本更新

--- t_customer.sql
+++ t_customer.sql
@@ -1,4 +1,32 @@
-INSERT INTO `t_customer`(`id`, `username`, `jobs`, `phone`) VALUES (1, 'asd', 'sadasd', 'asdsad');
-INSERT INTO `t_customer`(`id`, `username`, `jobs`, `phone`) VALUES (2, 'sad', 'asd', 'asd');
-INSERT INTO `t_customer`(`id`, `username`, `jobs`, `phone`) VALUES (3, 'asdasd', 'sadasd', 'sadasd');
-INSERT INTO `t_customer`(`id`, `username`, `jobs`, `phone`) VALUES (5, 'rose', 'student', '13333533092');
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost_3306
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost:3306
+ Source Schema         : mybatis
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : 65001
+
+ Date: 14/05/2018 16:22:07
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_customer
+-- ----------------------------
+DROP TABLE IF EXISTS `t_customer`;
+CREATE TABLE `t_customer`  (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `jobs` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
